e4492181866d88322a204140574ca6ee30892aa0
Yuri Cruz <yuricruzf@gmail.com>
2024-03-01 21:37:15 -0300

    Deleting reuse strategy

--- src/app/app-route-reuse-strategy.ts
+++ src/app/app-route-reuse-strategy.ts
@@ -1,8 +1,8 @@
-import {ActivatedRouteSnapshot, BaseRouteReuseStrategy} from '@angular/router';
+// import {ActivatedRouteSnapshot, BaseRouteReuseStrategy} from '@angular/router';
 
-export class AppRouteReuseStrategy extends BaseRouteReuseStrategy {
+// export class AppRouteReuseStrategy extends BaseRouteReuseStrategy {
 
-  public override shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return future.data['reuseComponent'];
-  }
-}
+//   public override shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+//     return future.data['reuseComponent'];
+//   }
+// }
